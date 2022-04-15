b9be99ad323fd8990eeb968a80ffb83e5733fa9a
Asghar <ali.itechpro@gmail.com>
2022-04-15 11:13:57 +1000

    files updated -2nd commit

--- js/index.js
+++ js/index.js
@@ -31,7 +31,7 @@ const getColletionDetails = () => {
                         <h2 class="title"><span class="oi oi-plus text-danger icon-title"></span>${item.Title}</h2>
                         <div class="para">${item.Intro}</div>
                         <br><br>
-                        <div class="read-time text-timer"><span class="oi oi-clock"> 2m &nbsp;</span> <span class="oi oi-comment-square"></span></div>`
+                        <div class="read-time text-timer"><span class="oi oi-clock"> ${item.Time} &nbsp;</span> <span class="oi oi-comment-square"></span></div>`
                         document.getElementById("mainItem").innerHTML = mainItem;
                     }
                     //sub news
@@ -39,7 +39,7 @@ const getColletionDetails = () => {
                         subItem += `<div class=""><img src="${item.Imageurl}" class="img-sub" alt="sports"></div>
                         <h2 class="title"><span class="oi oi-plus text-danger icon-title"></span>${item.Title}</h2>
                         <div>${item.Intro}</div>
-                        <div class="text-timer"><span class="oi oi-clock"></span> 1h &nbsp; <span class="oi oi-comment-square"></span>  1</div>
+                        <div class="text-timer"><span class="oi oi-clock"></span> ${item.Time}  &nbsp; <span class="oi oi-comment-square"></span>  1</div>
                         <hr class="mt-4 mb-4">`
                         document.getElementById("subItem").innerHTML = subItem;
                     }
@@ -50,7 +50,7 @@ const getColletionDetails = () => {
                     <div class="col-md-8  title-list pt-2 pb-2"><span class="oi oi-plus text-danger icon-list"> </span>${item.Title}</div>
                     <div class="col-md-4"><img src="${item.Imageurl}" alt="health" class="img-thumbnail img-list"></div>
                     <br/><br/>
-                    <div class="text-timer"><span class="oi oi-clock"></span>  1h</div>
+                    <div class="text-timer"><span class="oi oi-clock"></span>  ${item.Time} </div>
                     <hr class="mt-2 mb-2">
                     </div> `;
                     }
